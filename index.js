5c71b5998b65c1bf07cd292cf7a52cab1639a3b6
sultan5218 <sultan5218@gmail.com>
2022-11-08 00:28:03 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -6,6 +7,17 @@ require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+
+// mongodb start
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.newitlb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// mongodb end
 
 app.get("/", (req, res) => {
   res.send("Delivery service review api is running");
