59b6592c3613721006b45a2d1366284347d436dd
sultan nadim <sultan5218@gmail.com>
2022-11-09 15:14:29 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
@@ -18,12 +19,37 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function varifyJWT(req, res, next) {
+  const authHeader = req.headers.autorization;
+
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized User" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (error, decoded) {
+    if (error) {
+      return res.status(403).send({ message: "Unauthorized User" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
     const serviceCollaction = client
       .db("deliveryService")
       .collection("services");
     const reviewCollaction = client.db("deliveryService").collection("reviews");
+    // jwt token start
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+    // jwt token end
     // insert service
     app.post("/services", async (req, res) => {
       const service = req.body;
@@ -33,7 +59,7 @@ async function run() {
     // limit service find
     app.get("/service-limit", async (req, res) => {
       const query = {};
-      const cursor = serviceCollaction.find(query).limit(3);
+      const cursor = serviceCollaction.find(query).limit(3).sort({ _id: -1 });
       const result = await cursor.toArray();
       res.send(result);
     });
@@ -57,23 +83,40 @@ async function run() {
       const result = await reviewCollaction.insertOne(review);
       res.send(result);
     });
+
     // get service review
-    app.get("/reviews", async (req, res) => {
+    app.get("/reviews", varifyJWT, async (req, res) => {
+      const decoded = req.decoded;
+      if (decoded.email !== req.query.email) {
+        return res.status(401).send({ message: "Unauthorized User" });
+      }
+      let query = {};
+
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+
+      const cursor = reviewCollaction.find(query).sort({ _id: -1 });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // ========================
+    app.get("/service-reviews", async (req, res) => {
       let query = {};
       if (req.query.productId) {
         query = {
           productId: req.query.productId,
         };
       }
-      if (req.query.email) {
-        query = {
-          email: req.query.email,
-        };
-      }
+
       const cursor = reviewCollaction.find(query).sort({ _id: -1 });
       const result = await cursor.toArray();
       res.send(result);
     });
+    // ========================
     // delete review
     app.delete("/reviews/:id", async (req, res) => {
       const id = req.params.id;
