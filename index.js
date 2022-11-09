c1f116c33cfb8d5d0d1dda4fd99314c460b0489b
sultan nadim <sultan5218@gmail.com>
2022-11-09 19:38:01 +0600

    issue fix

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ async function run() {
     app.post("/jwt", (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
-        expiresIn: "1h",
+        expiresIn: "1d",
       });
       res.send({ token });
     });
